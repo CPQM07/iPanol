6a31db80de49b809974c42b3ccec3b12231efcaf
CPQM07 <cpqm07@gmail.com>
2017-06-12 20:56:02 -0400

    OTRA EEEEHH!

--- application/views/partialayout/csslayout.php
+++ application/views/partialayout/csslayout.php
@@ -3,6 +3,8 @@
   <link rel="stylesheet" href="<?= base_url('resources/css/bootstrap.css') ?>">
   <!-- font-awesome -->
   <link rel="stylesheet" href="<?= base_url('resources/css/font-awesome.css') ?>">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?= base_url('resources/css/dataTables.css') ?>">
   <!-- AdminLTE -->
   <link rel="stylesheet" href="<?= base_url('resources/css/AdminLTE.css') ?>">
   <!-- Ionicons -->
@@ -15,8 +17,6 @@
   <link rel="stylesheet" href="<?= base_url('resources/css/datepicker3.css') ?>">
   <!-- Select2 -->
   <link rel="stylesheet" href="<?= base_url('resources/css/select2.css') ?>">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="<?= base_url('resources/css/dataTables.css') ?>">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?= base_url('resources/css/AdminLTE.css') ?>">
   <!-- pNotify -->
