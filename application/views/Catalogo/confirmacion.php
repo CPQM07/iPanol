69f261191ad7388f0ade00d5375eadd142c84b21
yerkopailemilla <yerkopailemilla@gmail.com>
2017-06-14 22:54:38 -0400

    DETALLE CONFIRMACION PEDIDO1

--- application/views/Catalogo/confirmacion.php
+++ application/views/Catalogo/confirmacion.php
@@ -61,39 +61,71 @@
 		</div><!--/header-middle-->
 	</header>
 
+<!--
+	<div class="header-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">					
+					<div class="mainmenu pull-left">
+						<ul class="nav navbar-nav collapse navbar-collapse">
+							<li style="font-size: 23px;">Solicitud de artículos realizada correctamente.</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+-->    
 
+    <div class="header-bottom"><!--header-bottom-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">					
+					<div class="mainmenu pull-left">
+						<ul class="nav navbar-nav collapse navbar-collapse">
+							<li style="font-size: 21px;">N° de su pedido: <?= $ultimoID ?> </li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-3 pull-right">
+					<button class="btn btn-sucess pull-right"><a href="<?= site_url('/Catalogo/') ?>">Volver al inicio</a></button>	
+				</div>
+			</div>
+		</div>
+    </div>
 
-   <h2>N° de su pedido: <?= $ultimoID ?> </h2>
-   <hr>
-   <h3>Detalle de su solicitud</h3>
-
-   <table class="table table-striped">
-				<thead style="background: #FF1010; color: white;">
-					<tr style="font-size: 15px">
-						<th>ARTICULOS PEDIDOS</th>
-						<th></th>
-						<th>CANTIDAD</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php if (isset($_SESSION["productos"])): ?>
-					<?php foreach ($detalle as $key => $value): ?>
-					<tr style="font-size: 17px">
-						<td> <?= $value["nombre"]  ?></td>
-						<td></td>
-						<td><?= $value["cantidad"] ?></td>
-						<td></td>
-					</tr>
-					<?php endforeach ?>
-				<?php endif ?>
-				</tbody>
-			</table>
-
-   <button class="btn btn-danger"><a href="<?= site_url('/Catalogo/') ?>">Volver al inicio</button></a>
-
-
-
+    <div class="header-bottom"><!--header-bottom-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">	
+				<hr>										
+					<h3>Detalle de su solicitud</h3>
+		            <table class="table table-striped">
+						<thead style="background: #FF1010; color: white;">
+							<tr style="font-size: 15px">
+								<th>ARTICULOS PEDIDOS</th>
+								<th></th>
+								<th>CANTIDAD</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+						<?php if (isset($_SESSION["productos"])): ?>
+							<?php foreach ($detalle as $key => $value): ?>
+							<tr style="font-size: 17px">
+								<td> <?= $value["nombre"]  ?></td>
+								<td></td>
+								<td><?= $value["cantidad"] ?></td>
+								<td></td>
+							</tr>
+							<?php endforeach ?>
+						<?php endif ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+    </div>
 
     <div class="footer-bottom">
 			<div class="container">
